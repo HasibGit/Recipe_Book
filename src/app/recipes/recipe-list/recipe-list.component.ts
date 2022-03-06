c3d3e51a9b277573a98e3666701d7fdc1c4f69b2
HasibGit <48997902+HasibGit@users.noreply.github.com>
2022-03-07 00:30:00 +0600

    created recipe model and added dummy recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css', '../../../styles.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Biryani', 'Some sample description bla bla', 'https://vismaifood.com/storage/app/uploads/public/e12/7b7/127/thumb__1200_0_0_0_auto.jpg')
+  ];
 
   constructor() { }
 
