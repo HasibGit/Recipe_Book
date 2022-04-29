393d3b8e58b3b155833df49513f67b0ece434ba8
Hasib Ullah <walisantunu@gmail.com>
2022-04-30 01:03:17 +0600

    fetch ingredients implemented

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../services/recipe.service';
 
@@ -36,6 +36,7 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let imagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
@@ -43,12 +44,27 @@ export class RecipeEditComponent implements OnInit {
       recipeName = recipe.name;
       imagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe.ingredients) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              ingredientName: new FormControl(ingredient.name),
+              ingredientAmount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       Name: new FormControl(recipeName, [Validators.required]),
       ImagePath: new FormControl(imagePath, [Validators.required]),
       Description: new FormControl(recipeDescription, [Validators.required]),
+      Ingredients: recipeIngredients,
     });
+  }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('Ingredients')).controls;
   }
 }
